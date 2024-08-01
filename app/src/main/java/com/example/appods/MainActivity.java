81264ee0dd61f141acbaf626126625b033d8aef2
HenriqueCCruz <henriquecc2006@gmail.com>
2024-08-01 14:34:03 -0300

    Commit 01/08
    Não consegui testar, mas estou fazendo upload do que provavelmente estão funcionando. Testarei na sexta (2/08)

--- app/src/main/java/com/example/appods/MainActivity.java
+++ app/src/main/java/com/example/appods/MainActivity.java
@@ -1,14 +1,44 @@
-package com.example.appods;
+package com.example.atividade3;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu);
+
+        for (int i = 1; i <= 17; i++) {
+            String buttonID = "btn" + i;
+            int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+            Button button = findViewById(resID);
+            final int finalI = i;
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MenuActivity.this, getTargetActivity(finalI));
+                    startActivity(intent);
+                }
+            });
+        }
+    }
+
+    private Class<?> getTargetActivity(int buttonNumber) {
+        switch (buttonNumber) {
+            case 1:
+                return Activity1.class;
+            case 2:
+                return Activity2.class;
+            // Add cases for other buttons (3 to 16)
+            case 17:
+                return Activity17.class;
+            default:
+                return MenuActivity.class;
+        }
     }
 }
