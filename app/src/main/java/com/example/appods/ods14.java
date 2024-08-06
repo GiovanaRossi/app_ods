2cc6a919d97b82c22a1da8679d6f9c0e37cee856
Giovana Rossi <giovanarossi.gi@gmail.com>
2024-08-06 10:54:11 -0300

    Atualizando
    Limpei umas coisas, substitui o texto das ods.

--- app/src/main/java/com/example/appods/ods14.java
+++ app/src/main/java/com/example/appods/ods14.java
@@ -10,5 +10,6 @@ public class ods14 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ods14);
+
     }
 }
